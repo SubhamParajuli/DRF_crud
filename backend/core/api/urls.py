82e40ac37258514c2edcdd5957bd91fe04a26136
Subham Parajuli <suvamparajuli222@gmail.com>
2026-03-08 21:23:58 +0545

    compleded api for crud operations

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from task_app.views import *
 
 urlpatterns = [
-path('get_task/',get_task)
+    path('get_task/',get_task),
+    path('tasks/', TaskApi.as_view())
 ]
--- backend/core/task_app/views.py
+++ backend/core/task_app/views.py
@@ -1,9 +1,11 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Task
 from .serializers import TaskSerializer
+
 
 
 @api_view()
@@ -12,6 +14,65 @@ def get_task(request):
     serializer=TaskSerializer(queryset,many=True)
 
     return Response({
-        "data":serializer.data
+        "data":serializer.data,
     })
 
+
+
+class TaskApi(APIView):
+    def get(self,request):
+        queryset=Task.objects.all().order_by("-pk")
+        serializer=TaskSerializer(queryset,many=True)
+
+        return Response({
+            "data": serializer.data
+        })
+
+    def post(self,request):
+        data= request.data
+        serializer=TaskSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response({
+                "message": "data not saved.",
+                "errors": serializer.errors,
+            })
+        serializer.save()
+        return Response({
+            "message": "Data saved successfully."
+        })
+
+    def patch(self,request):
+        data=request.data
+
+        if not data.get('id'):
+            return Response({
+                "message": "data not updated",
+                "errors": "id not found"
+            })
+        
+        task= Task.objects.get(id=data.get('id'))
+        serializer=TaskSerializer(task, data=data, partial=True)
+        if not serializer.is_valid():
+            return Response({
+                "message": "data not updated",
+                "errors": serializer.errors,
+            })
+        serializer.save()
+        return Response({
+            "message": "Data updated successfully."
+        })
+
+
+    def delete(self,request):
+        data=request.data
+        if not data.get('id'):
+            return Response({
+                "message": "data not updated",
+                "errors": "id not found"
+            })
+        
+        task= Task.objects.get(id=data.get('id')).delete()
+        return Response({
+            "message": "Data deleted successfully."
+        })
